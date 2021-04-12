35a1ad4af159ed596c9130e2c76aaf09aaf026c2
maithevien9@gmail.com <maithevien123456789>
2021-04-12 23:17:38 +0700

    authentication created

---
+++ src/Page/Home/Dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+const Dashboard = () => {
+  return (
+    <View>
+      <Text>vien</Text>
+    </View>
+  );
+};
+export default Dashboard;
